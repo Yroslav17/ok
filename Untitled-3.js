70cf5e439c573ec09568b01aa2f827048e367aa3
Yroslav17 <112378671+Yroslav17@users.noreply.github.com>
2022-11-01 21:09:38 +0700

    Add files via upload

---
+++ Untitled-3.js
@@ -0,0 +1,132 @@
+var page = document.querySelector(".body");
+var themeButton = document.querySelector(".themeButton");
+
+themeButton.onclick = function() {
+page.classList.toggle("lightTheme");
+page.classList.toggle("darkTheme");
+};
+
+
+var burger = document.getElementById("mi");
+var mobNav = document.getElementById("mn");
+
+burger.onclick = function() {
+burger.classList.toggle("_active");
+mobNav.classList.toggle("_active");
+};
+
+function myFunction() {
+    /* Get the text field */
+    var copyText = document.getElementById("myInput");
+  
+    /* Select the text field */
+    copyText.select();
+  
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+  
+    /* Alert the copied text */
+    alert("Вы копируете: " + copyText.value);
+}
+
+function myFunction1() {
+    /* Get the text field */
+    var copyText = document.getElementById("myInput1");
+  
+    /* Select the text field */
+    copyText.select();
+  
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+  
+    /* Alert the copied text */
+    alert("Вы копируете: " + copyText.value);
+}
+
+function myFunction2() {
+    /* Get the text field */
+    var copyText = document.getElementById("myInput2");
+  
+    /* Select the text field */
+    copyText.select();
+  
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+  
+    /* Alert the copied text */
+    alert("Вы копируете: " + copyText.value);
+}
+
+function myFunction3() {
+    /* Get the text field */
+    var copyText = document.getElementById("myInput3");
+  
+    /* Select the text field */
+    copyText.select();
+  
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+  
+    /* Alert the copied text */
+    alert("Вы копируете: " + copyText.value);
+}
+
+function myFunction4() {
+    /* Get the text field */
+    var copyText = document.getElementById("myInput4");
+  
+    /* Select the text field */
+    copyText.select();
+  
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+  
+    /* Alert the copied text */
+    alert("Вы копируете: " + copyText.value);
+}
+
+function myFunction5() {
+    /* Get the text field */
+    var copyText = document.getElementById("myInput5");
+  
+    /* Select the text field */
+    copyText.select();
+  
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+  
+    /* Alert the copied text */
+    alert("Вы копируете: " + copyText.value);
+}
+
+function myFunction6() {
+    /* Get the text field */
+    var copyText = document.getElementById("myInput6");
+  
+    /* Select the text field */
+    copyText.select();
+  
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+  
+    /* Alert the copied text */
+    alert("Вы копируете: " + copyText.value);
+}
+
+function myFunction7() {
+    /* Get the text field */
+    var copyText = document.getElementById("myInput7");
+  
+    /* Select the text field */
+    copyText.select();
+  
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+  
+    /* Alert the copied text */
+    alert("Вы копируете: " + copyText.value);
+}
+
+
+
+
